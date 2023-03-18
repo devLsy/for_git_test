e9e32eec6e6518f925988eb94493abed8630d889
devlsy86 <infightersy@naver.com>
2023-03-18 20:44:44 +0900

    first commit

---
+++ src/main/java/study/springstudy30/SpringStudy30Application.java
@@ -0,0 +1,13 @@
+package study.springstudy30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudy30Application.class, args);
+    }
+
+}
---
+++ src/test/java/study/springstudy30/SpringStudy30ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springstudy30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy30ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
